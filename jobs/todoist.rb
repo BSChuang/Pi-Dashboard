23e66b28f331941dee892087887d5415b1a44448
Ben Chuang <ben10chuang@gmail.com>
2019-09-18 00:58:47 -0400

    Updated todoist API

--- jobs/todoist.rb
+++ jobs/todoist.rb
@@ -3,9 +3,9 @@ require "json"
 
 todoist_token = 'f04ea8df6e7df8b33bfb22d02c5ce2894c2fd204'
 
-SCHEDULER.every '5m', :first_in => 0 do |job|
+SCHEDULER.every '20m', :first_in => 0 do |job|
     
-    item_url_string  = 'https://todoist.com/API/v7/sync?token=' + todoist_token + '&resource_types=["items", "projects"]&sync_token=\'*\''
+    item_url_string  = 'https://todoist.com/API/v8/sync?token=' + todoist_token + '&resource_types=["items", "projects"]&sync_token=\'*\''
     encoded_item_url_string = URI.encode(item_url_string)
 
     item_uri = URI.parse(encoded_item_url_string)
@@ -26,16 +26,17 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
         end
 
         items = result['items']
-        puts items
+        #puts items
         items_array = Array[]
         hash = Hash.new
         items.each do |st|
-            dateString = st['date_string']
+            dateString = st['due']['date']
             if dateString != nil
                 date = Date.parse(dateString)
                 difference = (date - Date.today).to_i
-                puts projHash[st['project_id']].to_s
-                hash[dateString + " - " + projHash[st['project_id']].to_s + ": " +  + " " + st['content']] = difference
+		formatDate = date.strftime("%m/%d")
+                #puts projHash[st['project_id']].to_s
+                hash[formatDate + " - " + projHash[st['project_id']].to_s + ": " +  + " " + st['content']] = difference
             end
         end
         sorted = hash.sort_by { |name, date| date }
